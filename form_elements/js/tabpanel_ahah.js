abb9081eca7c84865c3da72b5f3e0e0ef72edc94
Nigel Banks <nigel.g.banks@gmail.com>
2011-04-14 11:07:52 -0300

    Changing the functionality of the tabpanels, now the icon in the first panel collapses/expands the tabs.

--- form_elements/js/tabpanel_ahah.js
+++ form_elements/js/tabpanel_ahah.js
@@ -1,27 +1,42 @@
 formbuilder = {
     tabpanel: {
+        tabs: null, // Collection of all tabpanels.
+        collapsibleTabs: null,
+        nonCollapsibleTabs: null,
         loadPanels: function () {
             var load = '.action-load-tabpanel';
             var collapsible = '.property-collapsible-tabpanel';
             var collapsed = '.property-collapsed-tabpanel';
-            var allTabs = $(load);
-            var collapsibleTabs = allTabs.filter(collapsible);
-            var nonCollapsibleTabs = allTabs.not(collapsible);
-            var expandedTabs = collapsibleTabs.not(collapsed);
-            var collapsedTabs = collapsibleTabs.filter(collapsed);
-            collapsedTabs.tabs( {collapsible: true, selected: -1 });
-            expandedTabs.tabs( {collapsible: true });
-            nonCollapsibleTabs.tabs({});
+            this.tabs = $(load);
+            this.collapsibleTabs = this.tabs.filter(collapsible);
+            this.nonCollapsibleTabs = this.tabs.not(collapsible);
+            var expandedTabs = this.collapsibleTabs.not(collapsed);
+            var collapsedTabs = this.collapsibleTabs.filter(collapsed);
+            collapsedTabs.tabs( { selected: -1 });
+            expandedTabs.tabs( { collapsible: true });
+            this.nonCollapsibleTabs.tabs({});
         },
         enableActions: function () {
-            $(".ui-icon-circle-plus").live("click", function() {
-                var id = $(this).text();
-                $("#"+id).trigger("mousedown");
+            $(".ui-icon-circle-triangle-e").live("click", function() {
+                var tabPanel = $(this).parents('div.ui-tabs:first');
+                $(this).removeClass('ui-icon-circle-triangle-e');
+                $(this).addClass('ui-icon-circle-triangle-s');
+                $("div.ui-tabs-panel", tabPanel).addClass("ui-tabs-hide");
+            });
+            $(".ui-icon-circle-triangle-s").live("click", function() {
+                var links = $(this).parents('ul:first');
+                var activeTab = $('li.ui-state-active:first', links).get(0);
+                var index = $('li', links).index(activeTab);
+                var tabPanel = $(this).parents('div.ui-tabs:first');
+                $('div.ui-tabs-panel:eq('+index+')', tabPanel).removeClass('ui-tabs-hide');
+                $(this).removeClass('ui-icon-circle-triangle-s');
+                $(this).addClass('ui-icon-circle-triangle-e');
             });
             $(".ui-icon-close").live("click", function() {
                 var id = $(this).text();
                 $("#"+id).trigger("mousedown");
             });
+            // TODO find Add button and Remove Button...
         }
     } 
 };
